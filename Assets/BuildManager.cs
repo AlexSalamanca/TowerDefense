9731b9bc8a3984e92b12e62e0766157bed6dd0b9
Alejandro Salamanca <alexsalamanca94@hotmail.com>
2019-07-04 17:21:22 -0700

    Added a building system

---
+++ Assets/BuildManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+    private GameObject turretToBuild;
+    public GameObject standardTurretToBuild;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("More than one build manager in scene");
+            return;
+        }
+        instance = this;
+    }
+    void Start()
+    {
+        turretToBuild = standardTurretToBuild;
+    }
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Assets/Node.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+    public Color hoverColor = Color.green;
+    private Renderer rend;
+    private Color startColor;
+    private GameObject turret;
+    public Vector3 offset;
+    
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+	void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+    void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Can't build there!");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + offset, transform.rotation);
+    }
+}
